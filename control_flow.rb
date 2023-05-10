07eb3dd8e965e9eb577d1e2f6e9ccdc81546f56c
KarimCarl <cndungu109@gmail.com>
2023-05-10 14:28:21 +0300

    Attempting new solutions as revision

--- control_flow.rb
+++ control_flow.rb
@@ -1,49 +1,99 @@
-def admin_login(username, password)
-  # your code here
-  # if username is "admin" or "ADMIN"
-  if ((username == "admin" || username == "ADMIN") && password == "12345")
-    "Access granted"
-  else
-    "Access denied"
-  end
+#conditionals 
+require 'pry'
+
+
+
+# If else/elsif statements
+#if condition is true
+# def task(day)
+#     if day == "Monday"
+#         "Go to school"
+#     elsif day == "Friday"
+#         "Go to Mosque"
+#     else
+#         "Make art!"
+#     end
+# end
+
+# binding.pry
+
+# task("Monday")
+
+
+#UNLESS 
+#Opposite of if/else statement is the "UNLESS" Statement
+#if condition is FALSE
+
+# def do_dishes (dishes)
+#     unless (dishes == "clean")
+#         "Do dishes!"
+#     else
+#         "Don't clean the dishes, they are clean!"
+#     end
+# end
+
+# binding.pry
+
+
+#CASE STATEMENT
+#Used for longer conditions
+# def todo_week day
+#     case day
+#     when "Monday"
+#         "Study!"
+#     when "Tuesday"
+#         "More studies"
+#     when "Wednesday"
+#         "Practice"
+#     when "Thursday"
+#         "Code challenge"
+#     else
+#         "Relax"
+#     end
+# end
+
+# binding.pry
+
+
+def admin_login (username, password)
+    if ((username == "admin" || username == "ADMIN") && (password == "12345"))
+        "Access granted"
+    else
+        "Access denied"
+    end
+end
+
+# binding.pry
+
+#weather
+def hows_the_weather (temperature)
+    if (temperature < 40)
+        "It's brisk out there!"
+    elsif ((temperature>=40) && (temperature<=65))
+        "It's a little chilly out there!"
+    elsif (temperature>85)
+        "It's too dang hot out there!"
+    else
+        "It's perfect out there!"
+    end
 end
 
 
-def hows_the_weather(temperature)
-  # your code here
-  if temperature < 40
-    "It's brisk out there!"
-  elsif temperature > 40 && temperature < 65
-    "It's a little chilly out there!"
-  elsif temperature > 85 
-    "It's too dang hot out there!"
-  else 
-    "It's perfect out there!"
-  end 
+#fizzbuzz
+def fizzbuzz (num)
+    if ((num%3==0)&&(num%5==0))
+        "FizzBuzz"
+    elsif (num%3 == 0)
+        "Fizz"
+    elsif (num%5 == 0)
+        "Buzz"
+    else
+        num
+    end
 end
 
-def fizzbuzz(num)
-  # your code here
-  if (num%3 == 0 && num%5 == 0)
-    "FizzBuzz"
-  elsif num%3 == 0
-    "Fizz"
-  elsif num%5 == 0
-    "Buzz"
-  else 
-    num
-  end
-end
 
-def calculator(operation, num1, num2)
-  # your code here
-  case operation 
-  when "+" then num1 + num2
-  when "-" then num1 - num2
-  when "*" then num1 * num2
-  when "/" then num1/num2
-  else
-    puts "Invalid operation!"
-    return nil
-  end
+#calculator
+def calculator(num1, num2, operation)
+
 end
